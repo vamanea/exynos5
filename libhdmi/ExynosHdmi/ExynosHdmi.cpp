d21eb9cde68f52bfe8b9b5064e0d855175a07695
Jason Lee <aznair.lee@samsung.com>
2012-10-08 10:43:38 +0900

    libhdmi: Adjust FB ion_map memory size
    Adjust buffer size used to ion_map FB memory to avoid G2D blit errors
    
    Change-Id: Ibc220cf4b16a191776e2848b4eaa86c4faf27ee7
    Signed-off-by: Jason Lee <aznair.lee@samsung.com>

--- libhdmi/ExynosHdmi/ExynosHdmi.cpp
+++ libhdmi/ExynosHdmi/ExynosHdmi.cpp
@@ -882,7 +882,7 @@ bool ExynosHdmi::m_Doflush(int srcW, int srcH, int srcColorFormat,
     }
 
     if (srcYAddr == 0 && mHdmiPath == HDMI_PATH_OVERLAY) {
-        unsigned int FB_size = ALIGN(srcW, 16) * ALIGN(srcH, 16) * HDMI_UI_BUFFER_BPP_SIZE * 2;
+        unsigned int FB_size = ALIGN(srcW, 16) * ALIGN(srcH, 16) * HDMI_UI_BUFFER_BPP_SIZE * 3;
         struct s3c_fb_user_ion_client ion_handle;
 
         if (0 < mFBaddr)
